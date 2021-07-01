60dec959070bf9b4960d8af3bd00948572b3bae6
JuanF Cattaneo <jfcattaneo87@gmail.com>
2021-06-30 20:56:47 -0500

    agregue barra a e commerce

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Juan <code>Cattaneo/ecommerce</code> tampugreen.
+          Juan <code>Cattaneo/e-commerce</code> tampugreen.
         </p>
         <a
           className="App-link"
